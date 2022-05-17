c70d62f81a96730997f642b1fa9d7de7e65e9af9
Ojeonghwan <ojh7086@gmail.com>
2022-05-17 22:36:59 +0900

    cors 설정 추가

--- src/main/java/com/ssafit/config/WebConfig.java
+++ src/main/java/com/ssafit/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafit.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,15 @@ import com.ssafit.interceptor.JWTInterceptor;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*")
+		.allowedMethods("GET","POST","PUT","DELETE")
+		.maxAge(6000);
+		
+	}
+	
+	
 	
 //	@Autowired
 //	JWTInterceptor JWTinterceptor;
