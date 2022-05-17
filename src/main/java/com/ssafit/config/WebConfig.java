eb906324ff6ede3f61b30901216affbb596650a7
Ojeonghwan <ojh7086@gmail.com>
2022-05-17 22:35:00 +0900

    JWT 로그인

--- src/main/java/com/ssafit/config/WebConfig.java
+++ src/main/java/com/ssafit/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.ssafit.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafit.interceptor.JWTInterceptor;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 	
+//	@Autowired
+//	JWTInterceptor JWTinterceptor;
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(JWTinterceptor).addPathPatterns("/**").excludePathPatterns("/api/login");
+//	}
 }
